1ebd800706186815c46d76fd8b488547ec2fd358
Vincent <v.wawak1@gmail.com>
2023-05-21 19:14:57 -0400

    Update to report.js name

--- src/commands/weather-commands/forecast.js
+++ src/commands/weather-commands/forecast.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('report')
+		.setName('forecast')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('pong!');
