de67951493f049fe778c7df60084d084bd436098
SpicyShark7713 <spicyshark7713@gmail.com>
2023-05-21 21:08:41 -0500

    Added some "details" to the ping command.

--- src/commands/general-commands/ping.js
+++ src/commands/general-commands/ping.js
@@ -1,10 +1,32 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('pong!');
+		try {
+			const requestOptions = {
+				method: 'GET',
+				redirect: 'follow'
+			  };
+			const response = await fetch(`https://cataas.com/cat?json=true`, requestOptions);
+			const data = await response.json();
+			const url = data.url;
+	
+	
+			const embed = new EmbedBuilder()
+				.setColor(0x0099FF)
+				.setTitle('Pong!')
+				.setDescription(`Get ponged.`)
+				.setImage(`https://cataas.com${url}`)
+				.setTimestamp()
+	
+			return interaction.reply({ embeds: [embed] });
+		}
+		catch(error) {
+			console.error("Error:", error);
+			return;
+		}
 	},
 };
